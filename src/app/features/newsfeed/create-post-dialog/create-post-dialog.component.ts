d7c74b65ca95b19abb4291c05bcfc65165488320
forsat <nove.ferenc@innostart.hu>
2026-02-15 15:43:47 +0100

    refactor(form): utolsó natív form elemek migrálása ps- komponensekre
    - invoice-create-dialog: 3x input + 1x select → ps-input + ps-select (tételsor)
    - create-post-dialog: <input type="time"> → ps-timepicker
    - Halott SCSS törölve: .text-input, .select-input, .number-input, .form-label, .field__input

--- src/app/features/newsfeed/create-post-dialog/create-post-dialog.component.ts
+++ src/app/features/newsfeed/create-post-dialog/create-post-dialog.component.ts
@@ -20,7 +20,7 @@ import { MediaEditorComponent, MediaEditorItem } from '../../../shared/component
 import { PostFormValidatorService, PostFormErrors } from '../services/post-form-validator.service';
 import { DialogWrapperComponent } from '../../../shared/components/dialog-wrapper/dialog-wrapper.component';
 import { CreatePostDialogActionsService } from './create-post-dialog-actions.service';
-import { PsInputComponent, PsDatepickerComponent } from '@shared/components/form';
+import { PsInputComponent, PsDatepickerComponent, PsTimepickerComponent } from '@shared/components/form';
 
 /**
  * Dialog eredmeny tipus
@@ -38,7 +38,7 @@ export type CreatePostResult =
  */
 @Component({
   selector: 'app-create-post-dialog',
-  imports: [FormsModule, LucideAngularModule, RichTextEditorComponent, MediaEditorComponent, DialogWrapperComponent, PsInputComponent, PsDatepickerComponent],
+  imports: [FormsModule, LucideAngularModule, RichTextEditorComponent, MediaEditorComponent, DialogWrapperComponent, PsInputComponent, PsDatepickerComponent, PsTimepickerComponent],
   templateUrl: './create-post-dialog.component.html',
   styleUrls: ['./create-post-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush

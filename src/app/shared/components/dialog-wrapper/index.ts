517e62abe5d101badb4ce357f86ab1c50e823b30
forsat <nove.ferenc@innostart.hu>
2026-02-20 16:59:09 +0100

    fix: dialog-wrapper barrel export (index.ts) hozzáadva
    A create-preliminary-modal és link-preliminary-dialog importjai
    '@shared/components/dialog-wrapper' path-ot használnak, amihez
    barrel export kell.

---
+++ src/app/shared/components/dialog-wrapper/index.ts
@@ -0,0 +1,2 @@
+export { DialogWrapperComponent } from './dialog-wrapper.component';
+export type { DialogConfig, DialogHeaderStyle, DialogSize, DialogTheme, DialogFooterAlign } from './dialog-wrapper.types';
